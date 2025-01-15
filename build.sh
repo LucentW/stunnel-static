85796e2d21cbf9a8070d386e6b4a9b3b1a52d86b
LucentW <335570+LucentW@users.noreply.github.com>
2025-01-15 13:42:37 +0000

    test 4

--- build.sh
+++ build.sh
@@ -28,9 +28,9 @@ file src/stunnel
 
 BIN_NAME="stunnel-$STUNNEL_VERSION-$DEST_ARCH-linux"
 
-mkdir -p ../dist
-mv src/stunnel ../dist/$BIN_NAME
+mkdir -p $GITHUB_WORKSPACE/dist
+mv src/stunnel $GITHUB_WORKSPACE/dist/$BIN_NAME
 
-cd ../dist
+cd $GITHUB_WORKSPACE/dist
 sha1sum $BIN_NAME > $BIN_NAME.sha1
 sha256sum $BIN_NAME > $BIN_NAME.sha256
